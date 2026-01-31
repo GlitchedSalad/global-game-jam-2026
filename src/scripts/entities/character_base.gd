a97daffccb4660b63abd8a15a3028bb036091af3
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-31 10:43:38 -0700

    added attack 2/whack to player

--- src/scripts/entities/character_base.gd
+++ src/scripts/entities/character_base.gd
@@ -2,6 +2,7 @@ class_name CharacterBase extends Node2D
 
 @export var controller : Node
 @export var attack1: Attack
+@export var attack2: Attack
 var direction: Vector2
 
 var health: HealthComponent
@@ -10,8 +11,10 @@ var health: HealthComponent
 func _ready() -> void:
 	controller = get_node("CharController")
 	controller.move.connect(_on_controller_move)
-	attack1 = get_node("Attack1")
+	#attack1 = get_node("Attack1")
+	#attack2 = get_node("Attack2")
 	controller.attack1.connect(_on_attack1)
+	controller.attack2.connect(_on_attack2)
 	direction = Vector2.RIGHT
 	
 	health = get_node_or_null("HealthComponent")
@@ -25,6 +28,9 @@ func _on_controller_move(vec : Vector2):
 
 func _on_attack1():
 	attack1.attack(direction)
+
+func _on_attack2():
+	attack2.attack(direction)
 	
 func _on_health_depleted():
 	print("dead")
--- src/scripts/entities/player_character.gd
+++ src/scripts/entities/player_character.gd
@@ -1,15 +1,17 @@
 class_name Player
 extends "res://src/scripts/entities/character_base.gd"
 
-@export var shield_node : Node
+@export var shield : Node
 
 func _ready() -> void:
-	controller.shield.connect(_on_controller_shield)
+	shield = get_node("Shield")
+	controller.shield.connect(_on_shield)
 	controller.move.connect(_on_controller_move)
 	controller.attack1.connect(_on_attack1)
+	controller.attack2.connect(_on_attack2)
 
 func add_xp(xp: int):
 	print("add me some xp")
 
-func _on_controller_shield():
+func _on_shield():
 	print("shield . do_something")
