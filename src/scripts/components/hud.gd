73ee0d36d9765800dd518bfe2eb100912bc237c1
Asparrow78 <sparrold@gmail.com>
2026-01-31 15:45:53 -0700

    added difficulty too the hud and global

--- src/scripts/components/hud.gd
+++ src/scripts/components/hud.gd
@@ -3,17 +3,21 @@ extends CanvasLayer
 @onready var points_label := $Points
 @onready var player_1_health_label := $player_1_health
 @onready var player_2_health_label := $player_2_health
+@onready var difficulty_label := $Difficulty
+
 
 func _ready() -> void:
 	Global.connect("points_changed", _on_points_changed)
 	Global.connect("player_1_health_change", _on_player_1_health_change)
 	Global.connect("player_1_health_change", _on_player_2_health_change)
+	Global.connect("difficulty_changed", _on_difficulty_changed)
 	points_label.text = ("Points: " + str(Global.points))
 	player_1_health_label.text = ("Player 1 Health: " + str(Global.Health_1))
 	if(Global.Health_2 != -99):
 		player_2_health_label.text = ("Player 2 Health: " + str(Global.Health_2))
 	else:
 		player_2_health_label.text = ""
+		_on_difficulty_changed() # initial update
 
 func _on_points_changed():
 	points_label.text = ("Points: " + str(Global.points))
@@ -30,3 +34,5 @@ func _on_player_2_health_change():
 	else:
 		player_2_health_label.text = ""
 	
+func _on_difficulty_changed() -> void:
+	difficulty_label.text = "Difficulty: " + str(snapped(Global.get_difficulty(), 0.1))
