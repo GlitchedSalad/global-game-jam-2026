08dfb0a1c6a929468d86b185059a91d0b552daf7
ThomasM <a02457532@aggies.usu.edu>
2026-01-31 15:29:25 -0700

    plaese god

---
+++ src/scripts/components/masks/mask_base.gd
@@ -0,0 +1,10 @@
+class_name Mask
+extends Resource 
+@export var speed_multiplier: float = 3.0
+@export var regen_multiplier: float = 1.0
+@export var damage_multiplier: float = 1.0
+@export var dash_multiplier: float = 1.0
+@export var special_attack: PackedScene
+func apply_to_controller(controller: Node) -> void:
+	if "speed" in controller:
+		controller.speed *= speed_multiplier
--- src/scripts/entities/player_character.gd
+++ src/scripts/entities/player_character.gd
@@ -1,10 +1,15 @@
 class_name Player
 extends CharacterBase
-
+@export var equipped_mask: Mask
 func _ready():
 	hit_layers = [1]
 	attack_layers = [0]
 	super._ready()
+	
+	if equipped_mask:
+		print("fuck")
+		equipped_mask.apply_to_controller(controller)
+#		special = equipped_mask.special_attack.instantiate()
 
 func add_xp(xp: int):
 	print("add me some xp")
