f40e88f87c79c28dc1d826302ac40fa9506e8187
ThomasM <a02457532@aggies.usu.edu>
2026-01-31 13:09:02 -0700

    added scene for magnet
    to do: add magnet to character scene

--- src/scripts/components/magnet_handler.gd
+++ src/scripts/components/magnet_handler.gd
@@ -1,5 +1,8 @@
 extends Area2D
-
+@export var size: int = 1:
+	set(value):
+		size = value
+		_on_size_change()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_size_change():
+	var child = get_node("CollisionShape2D")
+	child.scale = Vector2(size, size)
