2e5a889b2cf9a43d978e20ee85b1b25ace5584e5
ThomasM <a02457532@aggies.usu.edu>
2026-01-30 22:30:20 -0700

    added basic xp pickup

--- src/scripts/entities/pickup_handler.gd
+++ src/scripts/entities/pickup_handler.gd
@@ -12,5 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player"):
+	var player = area.get_parent()
+	if player.is_in_group("player"):
+		player.add_xp(10)
 		queue_free()
