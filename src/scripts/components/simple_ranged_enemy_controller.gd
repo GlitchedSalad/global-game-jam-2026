e32615baf070cf6a04776ae5e7282282a104ddf0
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-31 09:29:36 -0700

    made drag and drop melee and ranged enemies

--- src/scripts/components/simple_ranged_enemy_controller.gd
+++ src/scripts/components/simple_ranged_enemy_controller.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 @onready var player : Node2D = get_tree().get_first_node_in_group("player")
 
-@export var speed := 30.0
+@export var speed := 40.0
 @export var attack_range := 300.0
+@export var attack_time := 2.0
+
+@onready var timer := $Timer
 
 var direction : Vector2
 
@@ -17,6 +20,11 @@ func _ready() -> void:
 	if (player == null):
 		printerr("RangedEnemyController: missing player reference.")
 		return
+	
+	timer.wait_time = attack_time
+	timer.start()
+	timer.timeout.connect(_on_attack_timer_timeout)
+	
 
 func _process(delta: float) -> void:
 	if player == null:
@@ -24,3 +32,8 @@ func _process(delta: float) -> void:
 	direction = global_position.direction_to(player.global_position)
 	if ((global_position - player.global_position).length() > attack_range):
 		emit_signal("move", direction * speed * delta)
+	else:
+		emit_signal("move", direction * delta)
+
+func _on_attack_timer_timeout():
+	attack1.emit()
