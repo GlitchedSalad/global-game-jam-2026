b17892b94d247d66c5b7afd73df1a783eea7d30f
Ryan V <a02304400@usu.edu>
2026-01-31 10:51:13 -0700

    multi fireball and other small changes

--- src/scripts/entities/player_character.gd
+++ src/scripts/entities/player_character.gd
@@ -1,5 +1,5 @@
 class_name Player
-extends "res://src/scripts/entities/character_base.gd"
+extends CharacterBase
 
 @export var shield_node : Node
 
@@ -7,6 +7,7 @@ func _ready() -> void:
 	controller.shield.connect(_on_controller_shield)
 	controller.move.connect(_on_controller_move)
 	controller.attack1.connect(_on_attack1)
+	controller.attack2.connect(_on_attack2)
 
 func add_xp(xp: int):
 	print("add me some xp")
