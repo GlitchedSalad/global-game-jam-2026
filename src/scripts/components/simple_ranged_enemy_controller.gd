7f74d6a2862c4a51f40e287e99544797e9464d1c
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-30 22:51:20 -0700

    ranged enemy controller
    commented out shield till implementation

--- src/scripts/components/simple_ranged_enemy_controller.gd
+++ src/scripts/components/simple_ranged_enemy_controller.gd
@@ -2,23 +2,21 @@ extends Node2D
 
 @onready var player : Node2D = get_tree().get_first_node_in_group("player")
 
-@export var speed := 100.0
-@export var range := 30.0
+@export var speed := 30.0
+@export var attack_range := 300.0
+
 var direction : Vector2
 
 signal move(value : Vector2)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if (player == null):
 		printerr("RangedEnemyController: missing player reference.")
 		return
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if player == null:
 		return
 	direction = global_position.direction_to(player.global_position)
-	if ((global_position - player.global_position).length() > range):
+	if ((global_position - player.global_position).length() > attack_range):
 		emit_signal("move", direction * speed * delta)
--- src/scripts/entities/character_base.gd
+++ src/scripts/entities/character_base.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 @export var controller : Node
-@export var shield : Node
+@export var shield_node : Node
 
 
 func _ready() -> void:
 	controller.move.connect(_on_controller_move)
-	controller.shield.connect(_on_controller_shield)
+	#controller.shield.connect(_on_controller_shield)
 
 
 func _process(delta: float) -> void:
@@ -16,5 +16,5 @@ func _on_controller_move(vec : Vector2):
 	global_position += vec
 
 func _on_controller_shield():
-	print("shield.do_something")
+	print("shield . do_something")
 	pass
