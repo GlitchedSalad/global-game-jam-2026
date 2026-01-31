483f55d421ea2ccf53590600d196dc85965ec7ae
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-30 22:42:43 -0700

    Update simple_ranged_enemy_controller.gd

--- simple_ranged_enemy_controller.gd
+++ simple_ranged_enemy_controller.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@onready var player : Node2D = get_tree().get_first_node_in_group("player")
+
+@export var speed := 100.0
+@export var range := 30.0
+var direction : Vector2
+
+signal move(value : Vector2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if (player == null):
+		printerr("RangedEnemyController: missing player reference.")
+		return
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player == null:
+		return
+	direction = global_position.direction_to(player.global_position)
+	if ((global_position - player.global_position).length() > range):
+		emit_signal("move", direction * speed * delta)
