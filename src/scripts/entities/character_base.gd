7f7adc9baf3eb7a2433ef1b1a405e26b537787b1
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-31 11:27:20 -0700

    Revert "Merge branch 'main' into temp-assets"
    This reverts commit 8fafe9cf78613b6320dea774d9621715b4f1f56f, reversing
    changes made to 1ff75fdfdbcc48e847b5b336becedf66ea84e308.

--- src/scripts/entities/character_base.gd
+++ src/scripts/entities/character_base.gd
@@ -1,47 +1,25 @@
 class_name CharacterBase extends Node2D
 
-var controller : Node
-var health: HealthComponent
-var death: DeathComponent
-
-var attack1: Attack
-var attack2: Attack
-var special: Attack
-var shield
-
+@export var controller : Node
+@export var attack1: Attack
+@export var attack2: Attack
 var direction: Vector2
+
+var health: HealthComponent
 
 
 func _ready() -> void:
+	controller = get_node("CharController")
+	controller.move.connect(_on_controller_move)
+	#attack1 = get_node("Attack1")
+	#attack2 = get_node("Attack2")
+	controller.attack1.connect(_on_attack1)
+	controller.attack2.connect(_on_attack2)
 	direction = Vector2.RIGHT
-	
-	controller = get_node_or_null("CharController")
-	if (controller != null):
-		controller.move.connect(_on_controller_move)
 	
 	health = get_node_or_null("HealthComponent")
 	if (health is HealthComponent):
 		health.health_depleted.connect(_on_health_depleted)
-	
-	death = get_node_or_null("DeathComponent")
-	if (death is DeathComponent):
-		death.died.connect(_on_died)
-	
-	attack1 = get_node_or_null("Attack1")
-	if (attack1 is Attack):
-		controller.attack1.connect(_on_attack1)
-	
-	attack2 = get_node_or_null("Attack2")
-	if (attack2 is Attack):
-		controller.attack2.connect(_on_attack2)
-	
-	special = get_node_or_null("Special")
-	if (special is Attack):
-		controller.special.connect(_on_special)
-	
-	shield = get_node_or_null("Shield")
-	if (shield is Attack):
-		controller.shield.connect(_on_shield)
 
 func _on_controller_move(vec : Vector2):
 	global_position += vec
@@ -53,15 +31,6 @@ func _on_attack1():
 
 func _on_attack2():
 	attack2.attack(direction)
-
-func _on_special():
-	special.attack(direction)
-
-func _on_shield():
-	pass
-
+	
 func _on_health_depleted():
-	print("health gone")
-
-func _on_died():
-	print("died")
+	print("dead")
