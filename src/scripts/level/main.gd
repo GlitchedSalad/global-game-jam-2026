befcded0b63dc70f091881a34737948b7a492a03
Asparrow78 <sparrold@gmail.com>
2026-01-31 16:23:53 -0700

    finalized, enemy spawn rate with diff

--- src/scripts/level/main.gd
+++ src/scripts/level/main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var run_time := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	run_time += delta
+
+	var new_difficulty: float = min(
+		Global.difficulty_start + (run_time * Global.difficulty_growth_per_sec),
+		Global.difficulty_cap
+	)
+
+	if new_difficulty != Global.difficulty:
+		Global.difficulty = new_difficulty
+		Global.difficulty_changed.emit()
