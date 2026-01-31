e94a59be751c69d21860a1a3dd72cef2d48d3492
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-31 08:25:11 -0700

    added player character scene for default use

--- src/scripts/entities/character_base.gd
+++ src/scripts/entities/character_base.gd
@@ -1,7 +1,6 @@
-extends Node2D
+class_name CharacterBase extends Node2D
 
 @export var controller : Node
-@export var shield_node : Node
 @export var attack1: Attack
 var direction: Vector2
 
@@ -9,27 +8,14 @@ var direction: Vector2
 func _ready() -> void:
 	controller = get_node("CharController")
 	controller.move.connect(_on_controller_move)
-	#controller.shield.connect(_on_controller_shield)
 	attack1 = get_node("Attack1")
 	controller.attack1.connect(_on_attack1)
 	direction = Vector2.RIGHT
-
-
-func _process(delta: float) -> void:
-	pass
 
 func _on_controller_move(vec : Vector2):
 	global_position += vec
 	direction = vec
 
-func _on_controller_shield():
-	print("shield . do_something")
-	pass
-
-func add_xp(xp: int):
-	print("add me some xp")
-	pass
-	
 func _on_attack1():
 	attack1.attack(direction)
 	print(direction)
