951127ef787481dada5d99bcea5f2306a7b7f00a
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-30 21:07:23 -0700

    menu quit button

--- src/scripts/level/menu.gd
+++ src/scripts/level/menu.gd
@@ -9,7 +9,7 @@ func _on_help_button_pressed() -> void:
 	pass
 
 func _on_quit_button_pressed() -> void:
-	pass
+	get_tree().quit()
 
 func _on_play_button_pressed() -> void:
 	Global.change_scene(main_scene)
