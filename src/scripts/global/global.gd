c65c530bf60d1f2474598622c9825717b2a0f0f1
ThomasM <a02457532@aggies.usu.edu>
2026-01-31 09:29:08 -0700

    added healthbar for players

--- src/scripts/global/global.gd
+++ src/scripts/global/global.gd
@@ -3,8 +3,12 @@ extends Node
 # Global tracking variables
 var enemy_kills : int = 0
 var points : int = 0
+var Health_1 : int = 100
+var Health_2 : int = -99 #set to -99 to diasable
 
 signal points_changed
+signal player_1_health_change
+signal player_2_health_change
 
 func _ready():
 	process_mode = PROCESS_MODE_ALWAYS
@@ -12,6 +16,16 @@ func _ready():
 func add_points(amount : int):
 	points += amount
 	points_changed.emit()
+
+
+func update_health_1(health : int):
+	Health_1 = health
+	player_1_health_change.emit()
+
+
+func update_health_2(health : int):
+	Health_2 = health
+	player_2_health_change.emit()
 
 # Global functions
 
