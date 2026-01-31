0525b26c704d8ceb9fd485c58dea671575ef2270
ThomasM <a02457532@aggies.usu.edu>
2026-01-31 12:18:34 -0700

    added health ui and some magnet parts

--- src/scripts/components/hud.gd
+++ src/scripts/components/hud.gd
@@ -1,10 +1,32 @@
 extends CanvasLayer
 
 @onready var points_label := $Points
+@onready var player_1_health_label := $player_1_health
+@onready var player_2_health_label := $player_2_health
 
 func _ready() -> void:
 	Global.connect("points_changed", _on_points_changed)
+	Global.connect("player_1_health_change", _on_player_1_health_change)
+	Global.connect("player_1_health_change", _on_player_2_health_change)
 	points_label.text = ("Points: " + str(Global.points))
+	player_1_health_label.text = ("Player 1 Health: " + str(Global.Health_1))
+	if(Global.Health_2 != -99):
+		player_2_health_label.text = ("Player 2 Health: " + str(Global.Health_2))
+	else:
+		player_2_health_label.text = ""
 
 func _on_points_changed():
 	points_label.text = ("Points: " + str(Global.points))
+	
+
+func _on_player_1_health_change():
+	player_1_health_label.text = ("Player 1 Health: " + str(Global.Health_1))
+
+
+func _on_player_2_health_change():
+	return
+	if(Global.Health_2 != -99):
+		player_2_health_label.text = ("Player 2 Health: " + str(Global.Health_2))
+	else:
+		player_2_health_label.text = ""
+	
--- src/scripts/entities/pickup_handler.gd
+++ src/scripts/entities/pickup_handler.gd
@@ -2,6 +2,8 @@ class_name Pickup
 extends Node2D
 
 @export var xp: int = 10
+var magnet_pull := 300
+var target: Node2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if target:
+		global_position = global_position.move_toward(target.global_position,magnet_pull * delta)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("magnet"):
+		target = area
+		return
 	var player = area.get_parent()
 	if player.is_in_group("player"):
 		player.add_xp(10)
