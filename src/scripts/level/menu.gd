4d20f9dade556c26cb8ba28cc615f322016b7e50
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2026-01-30 20:43:46 -0700

    made menu buttons connect to script

--- src/scripts/level/menu.gd
+++ src/scripts/level/menu.gd
@@ -6,11 +6,10 @@ func _ready() -> void:
 	pass
 
 func _on_help_button_pressed() -> void:
-	pass # Replace with function body.
-
+	pass
 
 func _on_quit_button_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_play_button_pressed() -> void:
 	Global.change_scene(main_scene)
